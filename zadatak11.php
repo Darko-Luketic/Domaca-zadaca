fd86530baea12f8ef07ec1daac29b841621ac098
Darko-Luketic <83286304+Darko-Luketic@users.noreply.github.com>
2021-06-22 18:56:32 +0200

    Add files via upload

---
+++ zadatak11.php
@@ -0,0 +1,29 @@
+<?php
+
+// na stranici ispišite koristeći petlje po želji (nikako fiksno)
+
+/*
+
+4 x 10
+
+**********
+**********
+**********
+**********
+
+*/
+
+
+for($i=1;$i<=40;$i++){
+    echo '*';{
+        if($i===10 || $i===20){
+            echo '<br />';
+        }else if($i===30){
+            echo '<br />';
+        }
+    }
+}
+
+
+
+    
